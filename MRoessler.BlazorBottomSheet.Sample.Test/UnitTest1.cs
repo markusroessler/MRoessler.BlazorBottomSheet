521149a3a6939091517a2ef9a0782d417175a585
mroessler <roesslermarkus@gmail.com>
2026-01-25 19:38:35 +0100

    added playwright test

---
+++ MRoessler.BlazorBottomSheet.Sample.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright.NUnit;
+
+namespace MRoessler.BlazorBottomSheet.Sample.Test;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public async Task HasTitle()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+    }
+}
