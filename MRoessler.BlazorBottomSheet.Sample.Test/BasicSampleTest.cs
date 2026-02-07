1ecf0d750b02c77cb16bcc59e1b74353a37fad76
mroessler <roesslermarkus@gmail.com>
2026-02-07 23:15:52 +0100

    prepare test

--- MRoessler.BlazorBottomSheet.Sample.Test/BasicSampleTest.cs
+++ MRoessler.BlazorBottomSheet.Sample.Test/BasicSampleTest.cs
@@ -19,14 +19,12 @@ using NUnit.Framework.Interfaces;
 
 namespace MRoessler.BlazorBottomSheet.Sample.Test;
 
-[Parallelizable(ParallelScope.Self)]
 [TestFixture]
-public class BasicSampleTest : PageTest
+[Parallelizable(ParallelScope.Self)]
+public class BasicSampleTest : CustomPageTest
 {
     const int SlowDragStepDelayMs = 100;
     const int FastDragStepDelayMs = 0;
-
-    WebApplicationFactory<Program> _webAppFactory;
 
     ILocator _bottomSheet;
     ILocator _bottomSheetLayout;
@@ -35,22 +33,7 @@ public class BasicSampleTest : PageTest
     ILocator _footer;
     ILocator _handle;
     ILocator _openCloseButton;
-    TestHelper _testHelper;
 
-    [OneTimeSetUp]
-    public void OneTimeSetup()
-    {
-        _webAppFactory = new WebApplicationFactory<Program>();
-        _webAppFactory.UseKestrel(5001);
-        _webAppFactory.StartServer();
-        _testHelper = _webAppFactory.Services.GetRequiredService<TestHelper>();
-    }
-
-    [OneTimeTearDown]
-    public async Task OneTimeTeardownAsync()
-    {
-        await _webAppFactory.DisposeAsync();
-    }
 
     [SetUp]
     public void Setup()
@@ -64,61 +47,8 @@ public class BasicSampleTest : PageTest
         _openCloseButton = Page.GetByTestId("open-close-button");
     }
 
-    [TearDown]
-    public async Task TeardownAsync()
-    {
-        if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
-        {
-            var dir = Path.Combine(TestContext.CurrentContext.WorkDirectory, "Screenshots");
-            Directory.CreateDirectory(dir);
+    private Task<IResponse> GotoBasicSamplePageAsync() => Page.GotoAsync(WebAppFactory.ClientOptions.BaseAddress.ToString());
 
-            var fileName = $"failure_{TestContext.CurrentContext.Test.Name}.png";
-            var path = Path.Combine(dir, fileName);
-            await Page.ScreenshotAsync(new() { Path = path });
-            // TestContext.AddTestAttachment(path);
-        }
-    }
-
-    public override BrowserNewContextOptions ContextOptions()
-    {
-        return Playwright.Devices[Environment.GetEnvironmentVariable("DEVICE_NAME")];
-    }
-
-    private Task<IResponse> GotoBasicSamplePageAsync() => Page.GotoAsync("http://localhost:5001");
-
-    /// <summary>
-    /// note: can't use Teardown method for coverage export because it runs too late (Page-Context is gone)
-    /// </summary>
-    private async Task TestAsync(Func<Task> test)
-    {
-        try
-        {
-            await test();
-        }
-        finally
-        {
-            await TryExportCoverage();
-        }
-    }
-
-    [SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "it's a 'try'-method")]
-    private async Task TryExportCoverage()
-    {
-        try
-        {
-            var coverageJson = await Page.EvaluateAsync<ExpandoObject>("window.__coverage__");
-            if (coverageJson != null)
-            {
-                var outDir = Path.Combine(Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "..", ".."), ".nyc_output");
-                Directory.CreateDirectory(outDir);
-                File.WriteAllText(Path.Combine(outDir, $"{TestContext.CurrentContext.Test.MethodName}.json"), JsonSerializer.Serialize(coverageJson));
-            }
-        }
-        catch (Exception ex)
-        {
-            TestContext.Error.WriteLine("Failed to export coverage " + ex);
-        }
-    }
 
     [Test]
     public Task Test_SlowDragInDirection()
@@ -264,7 +194,7 @@ public class BasicSampleTest : PageTest
             var samplePageInstanceIdElm = await Page.GetByTestId("sample-instance-id").ElementHandleAsync();
             var samplePageInstanceId = Guid.Parse(await samplePageInstanceIdElm.TextContentAsync());
 
-            var samplePage = _testHelper.ActiveBasicSamplePages[samplePageInstanceId];
+            var samplePage = TestHelper.ActiveBasicSamplePages[samplePageInstanceId];
             var basicSampleViewModel = samplePage.ViewModel;
             var syncContextDispatcher = samplePage.SyncContextDispatcher;
 
---
+++ MRoessler.BlazorBottomSheet.Sample.Test/TwoSheetsSampleTest.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace MRoessler.BlazorBottomSheet.Sample.Test;
+
+[TestFixture]
+[Parallelizable(ParallelScope.Self)]
+public class TwoSheetsSampleTest : CustomPageTest
+{
+
+    ILocator _openCloseButton1;
+    ILocator _openCloseButton2;
+
+    ILocator _bottomSheetLayout1;
+    ILocator _bottomSheetLayout2;
+
+    ILocator _bottomSheet1;
+    ILocator _bottomSheet2;
+
+    ILocator _minimizedMarker1;
+    ILocator _minimizedMarker2;
+    ILocator _normalMarker1;
+    ILocator _normalMarker2;
+
+    [SetUp]
+    public void Setup()
+    {
+        _openCloseButton1 = Page.GetByTestId("open-close-button-1");
+        _openCloseButton2 = Page.GetByTestId("open-close-button-2");
+
+        _bottomSheetLayout1 = Page.GetByTestId("bottom-sheet-layout-1");
+        _bottomSheetLayout2 = Page.GetByTestId("bottom-sheet-layout-2");
+
+        _bottomSheet1 = _bottomSheetLayout1.GetByTestId("bottom-sheet");
+        _bottomSheet2 = _bottomSheetLayout2.GetByTestId("bottom-sheet");
+
+        _minimizedMarker1 = _bottomSheetLayout1.GetByTestId("minimized-marker");
+        _minimizedMarker2 = _bottomSheetLayout2.GetByTestId("minimized-marker");
+        _normalMarker1 = _bottomSheetLayout1.GetByTestId("normal-marker");
+        _normalMarker2 = _bottomSheetLayout2.GetByTestId("normal-marker");
+    }
+
+    private Task<IResponse> GotoSamplePageAsync() => Page.GotoAsync($"{WebAppFactory.ClientOptions.BaseAddress}two-sheets");
+
+    [Test]
+    public Task Test_Open()
+    {
+        return TestAsync(async () =>
+        {
+            await GotoSamplePageAsync();
+
+            await Expect(_bottomSheet1).Not.ToBeInViewportAsync();
+            await Expect(_bottomSheet1).ToContainClassAsync("closed");
+
+            await _openCloseButton1.ClickAsync();
+
+            await _bottomSheet1.WhenBoundsStable();
+            await Expect(_bottomSheet1).ToBeInViewportAsync();
+            await Expect(_bottomSheetLayout1).ToContainClassAsync("minimized");
+            await Expect(_minimizedMarker1).ToBeInViewportAsync();
+            await Expect(_normalMarker1).Not.ToBeInViewportAsync();
+        });
+    }
+}
