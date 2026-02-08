678062e636955ededdd9df6872284358d84b6f10
mroessler <32437037+markusroessler@users.noreply.github.com>
2026-02-09 00:42:29 +0100

    Feature/#15 lines of code badge (#19)
    * #15 export lines of code badge
    
    * fix script name
    
    * extract functions
    
    * fix script for bash
    
    * cleanup

---
+++ .github/scripts/export-lines-of-code-badge.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+scriptDir=$(dirname "$0")
+source "$scriptDir/lines-of-codes-lib.sh"
+
+allSourceCodeFiles=$(printAllSourceCodeFiles)
+libCount=$(printLibCount "$allSourceCodeFiles")
+badgeUri="https://img.shields.io/badge/Lines_of_Code-$libCount-blue"
+
+curl $badgeUri --output badges/badge_lines_of_code.svg
---
+++ .github/scripts/lines-of-codes-lib.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+printAllSourceCodeFiles() {
+    allSourceCodeFiles=$(git ls-files | grep -E '\.cs$|\.js$|\.razor$')
+    echo "$allSourceCodeFiles"
+}
+
+printLibCount() {
+    allSourceCodeFiles=$1
+    libCount=$(echo "$allSourceCodeFiles" | grep -E -v '.*Test/.*|Sample.*/' | xargs grep -v '^$' | wc -l)
+    echo $libCount
+}
+
+printAppCount() {
+    allSourceCodeFiles=$1
+    appCount=$(echo "$allSourceCodeFiles" | grep -E '.*Sample/' | xargs grep -v '^$' | wc -l)
+    echo $appCount
+}
+
+printTestCount() {
+    allSourceCodeFiles=$1
+    testCount=$(echo "$allSourceCodeFiles" | grep -E '.*Test/.*' | xargs grep -v '^$' | wc -l)
+    echo $testCount
+}
