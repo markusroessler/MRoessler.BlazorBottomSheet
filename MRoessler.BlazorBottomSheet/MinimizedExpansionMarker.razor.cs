134bb37ce577b95634848ea652b63a85d8c60809
mroessler <roesslermarkus@gmail.com>
2026-02-08 21:35:27 +0100

    ExpansionMarker adjustments

--- MRoessler.BlazorBottomSheet/MinimizedExpansionMarker.razor.cs
+++ MRoessler.BlazorBottomSheet/MinimizedExpansionMarker.razor.cs
@@ -10,9 +10,7 @@ namespace MRoessler.BlazorBottomSheet;
 /// <summary>
 /// Marker to be placed within the <see cref="BottomSheet"/> content for height-autosizing
 /// </summary>
-public partial class MinimizedExpansionMarker : ComponentBase
+public partial class MinimizedExpansionMarker
 {
-    [Parameter(CaptureUnmatchedValues = true)]
-    [SuppressMessage("Usage", "CA2227:Collection properties should be read only...", Justification = "...but not Blazor Parameters")]
-    public IDictionary<string, object>? AdditionalAttributes { get; set; }
+
 }
