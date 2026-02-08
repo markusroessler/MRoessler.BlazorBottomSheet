f4c8d90946820ceb5a2bfdbd73515b8121a5386b
mroessler <32437037+markusroessler@users.noreply.github.com>
2026-02-08 01:33:01 +0100

    Bugfix/#13 more than one sheet (#14)
    * #13 support more than one sheet
    
    * prepare test
    
    * move classes to subfolders
    
    * implement Test_Open
    
    * adjust locators
    
    * update MRoessler.DotnetAnalyzerConfig
    
    * rename classes
    
    * fix lines of code

--- MRoessler.BlazorBottomSheet.Sample.Test/TestSupport/LocatorExtensions.cs
+++ MRoessler.BlazorBottomSheet.Sample.Test/TestSupport/LocatorExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Playwright;
 
-namespace MRoessler.BlazorBottomSheet.Sample.Test;
+namespace MRoessler.BlazorBottomSheet.Sample.Test.TestSupport;
 
 public static class LocatorExtensions
 {
