faf4c833be385e3afb1d937444af8aa1f88f6b6e
mroessler <roesslermarkus@gmail.com>
2026-02-10 01:30:30 +0100

    extract function

--- .github/scripts/lines-of-codes-lib.sh
+++ .github/scripts/lines-of-codes-lib.sh
@@ -5,20 +5,24 @@ printAllSourceCodeFiles() {
     echo "$allSourceCodeFiles"
 }
 
+printCount() {
+    xargs grep -v '^$' | wc -l
+}
+
 printLibCount() {
     allSourceCodeFiles=$1
-    libCount=$(echo "$allSourceCodeFiles" | grep -E -v '.*Test/.*|Sample.*/' | xargs grep -v '^$' | wc -l)
+    libCount=$(echo "$allSourceCodeFiles" | grep -E -v '.*Test/.*|Sample.*/' | printCount)
     echo $libCount
 }
 
 printAppCount() {
     allSourceCodeFiles=$1
-    appCount=$(echo "$allSourceCodeFiles" | grep -E 'Sample.RazorComponents|Sample.Wasm|Sample.WebApp/' | xargs grep -v '^$' | wc -l)
+    appCount=$(echo "$allSourceCodeFiles" | grep -E 'Sample.RazorComponents|Sample.Wasm|Sample.WebApp/' | printCount)
     echo $appCount
 }
 
 printTestCount() {
     allSourceCodeFiles=$1
-    testCount=$(echo "$allSourceCodeFiles" | grep -E '.*Test/.*' | xargs grep -v '^$' | wc -l)
+    testCount=$(echo "$allSourceCodeFiles" | grep -E '.*Test/.*' | printCount)
     echo $testCount
 }
