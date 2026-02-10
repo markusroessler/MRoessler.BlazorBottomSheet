1f63adb0dc5a4dcefd51aeb7a2f1ad02a867606c
mroessler <roesslermarkus@gmail.com>
2026-02-10 01:27:27 +0100

    fix "lines of code"

--- .github/scripts/lines-of-codes-lib.sh
+++ .github/scripts/lines-of-codes-lib.sh
@@ -13,7 +13,7 @@ printLibCount() {
 
 printAppCount() {
     allSourceCodeFiles=$1
-    appCount=$(echo "$allSourceCodeFiles" | grep -E '.*Sample/' | xargs grep -v '^$' | wc -l)
+    appCount=$(echo "$allSourceCodeFiles" | grep -E 'Sample.RazorComponents|Sample.Wasm|Sample.WebApp/' | xargs grep -v '^$' | wc -l)
     echo $appCount
 }
 
