ba1a4fce8217c86e6d8656fdd667b822821121c9
mroessler <roesslermarkus@gmail.com>
2026-07-19 14:34:16 +0200

    update dotnet tools

--- .github/scripts/collect-build-results.sh
+++ .github/scripts/collect-build-results.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet export-dotnet-results-for-github --export-step-summary true --github-server-url $1 --github-repo $2 --github-ref-name $3 --culture "de-DE"
+dotnet export-dotnet-results-for-github --export-step-summary true --github-server-url $1 --github-repo $2 --github-ref-name $3 --step-summary-content-types $4 --culture "de-DE"
